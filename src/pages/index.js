7d8fedaa1ae46a233cae55a6ac5636e337303cca
guoshencheng <648772021@qq.com>
2018-09-17 11:44:05 +0800

    change some style

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,25 +4,33 @@ import MashRoom from '../components/mashroom';
 import './index.less';
 
 const IndexPage = () => (
-  <div className="home-page">
-    <h1 className="game-text">
-      Century's World
-    </h1>
-    <div className="mashroom-container">
-      <MashRoom />
-    </div>
-    <div className="operations game-text">
-      <a href="">
-        Post
-      </a>
-      <a href="">
-        Tips
-      </a>
-      <a href="">
-        Github
-      </a>
-    </div>
-  </div>
+  <table className="home-page-container">
+    <tbody>
+      <tr>
+        <td>
+          <div className="home-page">
+            <div className="game-text home-title">
+              Century's World
+            </div>
+            <div className="mashroom-container">
+              <MashRoom />
+            </div>
+            <div className="operations game-text">
+              <Link to="/posts">
+                Posts
+              </Link>
+              <Link to="">
+                Tips
+              </Link>
+              <Link to="">
+                Github
+              </Link>
+            </div>
+          </div>
+        </td>
+      </tr>
+    </tbody>
+  </table>
 )
 
 export default IndexPage
