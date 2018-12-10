44ac4f1cfb7b90f8db04fd8cdea89a8f030c6216
guoshencheng (from Travis CI) <648772021@qq.com>
2018-12-10 15:16:08 +0000

    Deploy guoshencheng/CenturyGuo to github.com/guoshencheng/CenturyGuo.git:gh-pages

---
+++ path---posts-wepy-88cf11c1a9cf9d0e8900.js
@@ -0,0 +1,2 @@
+webpackJsonp([0x8d316447dc3f],{381:function(e,o){e.exports={data:{allMarkdownRemark:{totalCount:1,edges:[{node:{id:"/home/travis/build/guoshencheng/CenturyGuo/docs/shallow-into-wepy-source-code.md absPath of file >>> MarkdownRemark",frontmatter:{title:"浅析wepy源码",date:"23 May, 2018"},fields:{slug:"shallow-into-wepy-source-code"},excerpt:"首先让我们先恭喜RNG，其次我们恭喜LGD(data, lol的玩家一个都不能得罪)。wepy是腾讯自家出品的一个小程序框架。小程序在代码体验上会和vue差不多，很多人从vue转向小程序可以说门槛非常低，但是小程序的开发体验我觉得一直是个问题，样式的支持、js…"}}]}},pathContext:{tag:"/wepy/"}}}});
+//# sourceMappingURL=path---posts-wepy-88cf11c1a9cf9d0e8900.js.map
